3dd8f53a4f1d26f9cff91715f92da5f5c90a9cda
Murilo Pavanello <murilopava@gmail.com>
2026-09-04 20:50:02 -0300

    alterar aula 1 e 4 para exercícios

--- aula04/assets/scripts/resumo.js
+++ aula04/assets/scripts/resumo.js
@@ -2,10 +2,29 @@ const carregarEndereco = function () {
     let endereco = JSON.parse(
         sessionStorage.getItem('endereco')
     );
-    document.getElementById('rua').innerText = endereco.rua;
-    document.getElementById('cidade').innerText = endereco.cidade;
-    document.getElementById('estado').innerText = endereco.estado;
-    document.getElementById('cep').innerText = endereco.cep;
+    
+    if (endereco) {
+        document.getElementById('rua').innerText = endereco.rua;
+        document.getElementById('cidade').innerText = endereco.cidade;
+        document.getElementById('estado').innerText = endereco.estado;
+        document.getElementById('cep').innerText = endereco.cep;
+    }
+}
+
+const carregarProdutos = function () {
+    let produtos = JSON.parse(
+        sessionStorage.getItem('produtos')
+    );
+
+    if (produtos) {
+
+        document.getElementById('res_hav_quantidade').innerText = produtos.havaianas.quantidade;
+        document.getElementById('res_hav_preco').innerText = produtos.havaianas.preco;
+
+        document.getElementById('res_alp_quantidade').innerText = produtos.alpargatas.quantidade;
+        document.getElementById('res_alp_preco').innerText = produtos.alpargatas.preco;
+    }
 }
 
 carregarEndereco();
+carregarProdutos();
